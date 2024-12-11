4794419e348bc59b4abc76a9eca96ef5accd268a
wuttiwong <wutkmutt@gmail.com>
2024-12-11 09:07:22 +0700

    First commit

---
+++ src/app/my-modal/my-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-my-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-modal.component.html',
+  styleUrl: './my-modal.component.css'
+})
+export class MyModalComponent {
+  @Input() modalId: string = '';
+  @Input() title: string = '';
+  @Input() modalSize: string = '';
+
+}
---
+++ src/config.ts
@@ -0,0 +1,7 @@
+import { appConfig } from "./app/app.config";
+
+const config = {
+    apiServer: 'http://localhost:3000',
+};
+
+export default config;
