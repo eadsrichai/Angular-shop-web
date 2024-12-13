2df4626e8e6d2a9ae19833b34115647eed698f1e
wuttiwong <wutkmutt@gmail.com>
2024-12-13 15:00:10 +0700

    taste

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { FoodTypeComponent } from './food-type/food-type.component';
 import { FoodSizeComponent } from './food-size/food-size.component';
+import { TasteComponent } from './taste/taste.component';
 
 export const routes: Routes = [
     {
@@ -16,4 +17,8 @@ export const routes: Routes = [
         path: 'foodSize',
         component: FoodSizeComponent,
     },
+    {
+        path: 'taste',
+        component: TasteComponent
+    }
 ];
